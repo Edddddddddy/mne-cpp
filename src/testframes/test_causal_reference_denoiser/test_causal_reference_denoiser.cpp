19a23b9a654d59794dc12140229de6726671d831
Edddddddddy <983166955@qq.com>
2026-08-05 03:39:18 +0800

    test(rtprocessing): cover invalid denoiser configurations

--- src/testframes/test_causal_reference_denoiser/test_causal_reference_denoiser.cpp
+++ src/testframes/test_causal_reference_denoiser/test_causal_reference_denoiser.cpp
@@ -12,6 +12,8 @@
 
 #include <Eigen/Dense>
 
+#include <limits>
+
 //=============================================================================================================
 // QT INCLUDES
 //=============================================================================================================
@@ -34,6 +36,8 @@ class TestCausalReferenceDenoiser : public QObject
 private slots:
     void configureAndBypassTrackHistory();
     void rejectedConfigurationPreservesCommittedShape();
+    void rejectsInvalidConfiguration_data();
+    void rejectsInvalidConfiguration();
 };
 
 //=============================================================================================================
@@ -124,6 +128,129 @@ void TestCausalReferenceDenoiser::rejectedConfigurationPreservesCommittedShape()
     QCOMPARE(committedBlock.rows(), expectedCommittedBlock.rows());
     QCOMPARE(committedBlock.cols(), expectedCommittedBlock.cols());
     QVERIFY((committedBlock.array() == expectedCommittedBlock.array()).all());
+}
+
+//=============================================================================================================
+
+void TestCausalReferenceDenoiser::rejectsInvalidConfiguration_data()
+{
+    QTest::addColumn<QString>("mutation");
+
+    QTest::newRow("samplingFrequencyHz_zero") << QStringLiteral("samplingFrequencyHz_zero");
+    QTest::newRow("samplingFrequencyHz_negative") << QStringLiteral("samplingFrequencyHz_negative");
+    QTest::newRow("samplingFrequencyHz_nan") << QStringLiteral("samplingFrequencyHz_nan");
+    QTest::newRow("samplingFrequencyHz_positiveInfinity") << QStringLiteral("samplingFrequencyHz_positiveInfinity");
+    QTest::newRow("tapCount_zero") << QStringLiteral("tapCount_zero");
+    QTest::newRow("tapCount_negative") << QStringLiteral("tapCount_negative");
+    QTest::newRow("adaptationIntervalSamples_zero") << QStringLiteral("adaptationIntervalSamples_zero");
+    QTest::newRow("adaptationIntervalSamples_negative") << QStringLiteral("adaptationIntervalSamples_negative");
+    QTest::newRow("memoryTimeSeconds_zero") << QStringLiteral("memoryTimeSeconds_zero");
+    QTest::newRow("memoryTimeSeconds_negative") << QStringLiteral("memoryTimeSeconds_negative");
+    QTest::newRow("memoryTimeSeconds_nan") << QStringLiteral("memoryTimeSeconds_nan");
+    QTest::newRow("memoryTimeSeconds_positiveInfinity") << QStringLiteral("memoryTimeSeconds_positiveInfinity");
+    QTest::newRow("regularization_belowMinimum") << QStringLiteral("regularization_belowMinimum");
+    QTest::newRow("regularization_aboveMaximum") << QStringLiteral("regularization_aboveMaximum");
+    QTest::newRow("regularization_nan") << QStringLiteral("regularization_nan");
+    QTest::newRow("regularization_positiveInfinity") << QStringLiteral("regularization_positiveInfinity");
+    QTest::newRow("referenceRows_empty") << QStringLiteral("referenceRows_empty");
+    QTest::newRow("targetRows_empty") << QStringLiteral("targetRows_empty");
+    QTest::newRow("referenceRows_duplicate") << QStringLiteral("referenceRows_duplicate");
+    QTest::newRow("targetRows_duplicate") << QStringLiteral("targetRows_duplicate");
+    QTest::newRow("referenceRows_negative") << QStringLiteral("referenceRows_negative");
+    QTest::newRow("targetRows_negative") << QStringLiteral("targetRows_negative");
+    QTest::newRow("referenceRows_equalChannelCount") << QStringLiteral("referenceRows_equalChannelCount");
+    QTest::newRow("targetRows_equalChannelCount") << QStringLiteral("targetRows_equalChannelCount");
+    QTest::newRow("referenceRows_targetRows_overlap") << QStringLiteral("referenceRows_targetRows_overlap");
+    QTest::newRow("referenceRows_tapCount_featureLimit") << QStringLiteral("referenceRows_tapCount_featureLimit");
+}
+
+//=============================================================================================================
+
+void TestCausalReferenceDenoiser::rejectsInvalidConfiguration()
+{
+    QFETCH(QString, mutation);
+
+    CausalReferenceDenoiserConfig candidate;
+    candidate.samplingFrequencyHz = 1000.0;
+    candidate.channelCount = 3;
+    candidate.maxBlockSamples = 4;
+    candidate.referenceRows = VectorXi(1);
+    candidate.referenceRows << 0;
+    candidate.targetRows = VectorXi(2);
+    candidate.targetRows << 1, 2;
+    candidate.tapCount = 4;
+    candidate.adaptationIntervalSamples = 128;
+    candidate.memoryTimeSeconds = 30.0;
+    candidate.regularization = 1e-3;
+
+    const double quietNaN = std::numeric_limits<double>::quiet_NaN();
+    const double positiveInfinity = std::numeric_limits<double>::infinity();
+
+    if (mutation == QStringLiteral("samplingFrequencyHz_zero")) {
+        candidate.samplingFrequencyHz = 0.0;
+    } else if (mutation == QStringLiteral("samplingFrequencyHz_negative")) {
+        candidate.samplingFrequencyHz = -1.0;
+    } else if (mutation == QStringLiteral("samplingFrequencyHz_nan")) {
+        candidate.samplingFrequencyHz = quietNaN;
+    } else if (mutation == QStringLiteral("samplingFrequencyHz_positiveInfinity")) {
+        candidate.samplingFrequencyHz = positiveInfinity;
+    } else if (mutation == QStringLiteral("tapCount_zero")) {
+        candidate.tapCount = 0;
+    } else if (mutation == QStringLiteral("tapCount_negative")) {
+        candidate.tapCount = -1;
+    } else if (mutation == QStringLiteral("adaptationIntervalSamples_zero")) {
+        candidate.adaptationIntervalSamples = 0;
+    } else if (mutation == QStringLiteral("adaptationIntervalSamples_negative")) {
+        candidate.adaptationIntervalSamples = -1;
+    } else if (mutation == QStringLiteral("memoryTimeSeconds_zero")) {
+        candidate.memoryTimeSeconds = 0.0;
+    } else if (mutation == QStringLiteral("memoryTimeSeconds_negative")) {
+        candidate.memoryTimeSeconds = -1.0;
+    } else if (mutation == QStringLiteral("memoryTimeSeconds_nan")) {
+        candidate.memoryTimeSeconds = quietNaN;
+    } else if (mutation == QStringLiteral("memoryTimeSeconds_positiveInfinity")) {
+        candidate.memoryTimeSeconds = positiveInfinity;
+    } else if (mutation == QStringLiteral("regularization_belowMinimum")) {
+        candidate.regularization = 0.0;
+    } else if (mutation == QStringLiteral("regularization_aboveMaximum")) {
+        candidate.regularization = 1.1;
+    } else if (mutation == QStringLiteral("regularization_nan")) {
+        candidate.regularization = quietNaN;
+    } else if (mutation == QStringLiteral("regularization_positiveInfinity")) {
+        candidate.regularization = positiveInfinity;
+    } else if (mutation == QStringLiteral("referenceRows_empty")) {
+        candidate.referenceRows = VectorXi(0);
+    } else if (mutation == QStringLiteral("targetRows_empty")) {
+        candidate.targetRows = VectorXi(0);
+    } else if (mutation == QStringLiteral("referenceRows_duplicate")) {
+        candidate.referenceRows = VectorXi(2);
+        candidate.referenceRows << 0, 0;
+    } else if (mutation == QStringLiteral("targetRows_duplicate")) {
+        candidate.targetRows = VectorXi(3);
+        candidate.targetRows << 1, 2, 2;
+    } else if (mutation == QStringLiteral("referenceRows_negative")) {
+        candidate.referenceRows = VectorXi(1);
+        candidate.referenceRows << -1;
+    } else if (mutation == QStringLiteral("targetRows_negative")) {
+        candidate.targetRows = VectorXi(2);
+        candidate.targetRows << -1, 2;
+    } else if (mutation == QStringLiteral("referenceRows_equalChannelCount")) {
+        candidate.referenceRows = VectorXi(1);
+        candidate.referenceRows << candidate.channelCount;
+    } else if (mutation == QStringLiteral("targetRows_equalChannelCount")) {
+        candidate.targetRows = VectorXi(2);
+        candidate.targetRows << 1, candidate.channelCount;
+    } else if (mutation == QStringLiteral("referenceRows_targetRows_overlap")) {
+        candidate.targetRows = VectorXi(2);
+        candidate.targetRows << 0, 2;
+    } else if (mutation == QStringLiteral("referenceRows_tapCount_featureLimit")) {
+        candidate.tapCount = 257;
+    } else {
+        QFAIL("Unknown invalid configuration mutation");
+    }
+
+    CausalReferenceDenoiser denoiser;
+    QVERIFY(denoiser.configure(candidate) == DenoiserStatus::InvalidConfiguration);
 }
 
 //=============================================================================================================
